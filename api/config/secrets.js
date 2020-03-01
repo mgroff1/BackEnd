7e4d96b1440624dc52c8da3500df75c3498f9399
Mick Groff <mick.groff@gmail.com>
2020-03-01 16:52:32 -0700

    --secret--I am working with postman fo get docs

--- api/config/secrets.js
+++ api/config/secrets.js
@@ -1,8 +1,4 @@
 module.exports = {
-<<<<<<< Updated upstream
   jwtSecret: process.env.JWT_SECRET || 'Im so embarassed you almost saw my secret!',
-=======
-  jwtSecret: process.env.JWT_SECRET || 'Im so embaraced you almost saw my secret!',
->>>>>>> Stashed changes
 };
   

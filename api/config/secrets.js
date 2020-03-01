f2b27c3f4d6f4cf29747658131084146f18b498e
Mick Groff <mick.groff@gmail.com>
2020-03-01 16:44:36 -0700

    Update secrets.js

--- api/config/secrets.js
+++ api/config/secrets.js
@@ -1,4 +1,8 @@
 module.exports = {
+<<<<<<< Updated upstream
   jwtSecret: process.env.JWT_SECRET || 'Im so embarassed you almost saw my secret!',
+=======
+  jwtSecret: process.env.JWT_SECRET || 'Im so embaraced you almost saw my secret!',
+>>>>>>> Stashed changes
 };
   

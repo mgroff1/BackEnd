d23f3e29e72aaeeabe8188604318a1ac964a4596
Mick Groff <mick.groff@gmail.com>
2020-03-03 20:42:21 -0700

    --

--- knexfile.js
+++ knexfile.js
@@ -23,24 +23,8 @@ module.exports = {
     },
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
-    client: 'pg',
+    client: 'sqlite3',
     connection: productionDbConnection,
     useNullAsDefault: true,
     migrations: {

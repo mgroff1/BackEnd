e5d05f43a13daf284cd06858b7ebc51f65d2cd9b
Mick Groff <mick.groff@gmail.com>
2020-03-05 15:29:21 -0700

    --

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ const server = require('./api/server.js');
 
 const port = process.env.PORT || 5150;
  server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+
+ document.getElementById('body').innerHTML = '< button class = "button" > < a style = "font-size:3.2rem; background:red; color: black; border-radius:7px;"'+
+'href = \'./index.html\' > SEE API DOC < /a></button >'
